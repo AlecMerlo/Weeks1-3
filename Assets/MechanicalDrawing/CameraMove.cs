8e45088a492cb419334b08b245c9711e41f816ed
Alec Merlo <merloa@shernet.sheridancollege.ca>
2025-01-21 20:11:49 -0500

    camera rotation

--- Assets/MechanicalDrawing/CameraMove.cs
+++ Assets/MechanicalDrawing/CameraMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -10,6 +11,21 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.mousePosition.x < (Screen.width / 2) - (Screen.width / 4) && transform.rotation.y > -0.2f * Mathf.Deg2Rad)
+        {
+            transform.Rotate(0, -0.1f * Mathf.Deg2Rad, 0);
+        }
+        else if (Input.mousePosition.x > (Screen.width / 2) + (Screen.width / 4) && transform.rotation.y < 0.2f * Mathf.Deg2Rad)
+        {
+            transform.Rotate(0, 0.1f * Mathf.Deg2Rad, 0);
+        }
+        if (Input.mousePosition.y < (Screen.height / 2) - (Screen.height / 4) && transform.rotation.x < 0.2f * Mathf.Deg2Rad)
+        {
+            transform.Rotate(0.1f * Mathf.Deg2Rad, 0, 0);
+        }
+        else if (Input.mousePosition.y > (Screen.height / 2) + (Screen.height / 4) && transform.rotation.x > -0.2f * Mathf.Deg2Rad)
+        {
+            transform.Rotate(-0.1f * Mathf.Deg2Rad, 0, 0);
+        }
     }
 }
